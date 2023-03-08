3f2171c32ea9e60d3ffa20e4f5f12365b58d1521
Shallon Kobusinge <kobshallon041@gmail.com>
2023-03-08 14:41:04 +0200

    chore: configure dotenv

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', function (req, res) {
     res.send(`${process.env.MESSAGE}`);
 });
+
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });
