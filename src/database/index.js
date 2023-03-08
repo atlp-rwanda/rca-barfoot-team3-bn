5cd311a9ec7eaeb51e86fc6f0843aeba2ac10c19
Irakoze-Hortance <hortanceirakoze@gmail.com>
2023-03-08 14:43:06 +0200

    added database configurations locally

--- src/database/index.js
+++ src/database/index.js
@@ -1,1 +1,12 @@
 // database configurations here
+const {Client}= require ("pg")
+
+const client = new Client({
+    user:'postgres',
+    database:'challenge',
+    password:'admin',
+    port:5432,
+    host:'localhost'
+})
+
+module.exports= {client}
