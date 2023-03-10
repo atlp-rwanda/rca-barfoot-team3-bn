7ffdffe58dd301e5040deb79b87c8cbe7ea53aec
tracy8 <tracymurenzi@gmail.com>
2023-03-10 14:56:08 +0200

    fix: fixed linter errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     "one-var-declaration-per-line": 0,
     "new-cap": 0,
     "consistent-return": 0,
-    "no-param-reassign": '0',
+    "no-param-reassign": 0,
     "comma-dangle": 0,
     "curly": ["error", "multi-line"],
     "import/no-unresolved": [2, { commonjs: true }],
