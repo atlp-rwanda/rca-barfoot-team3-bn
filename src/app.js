5874735ce793189984edec00e16d9dc26e75aac4
DeliceLydia <Delydia84@gmail.comm>
2023-03-15 10:04:08 +0200

    Fix linters

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
-
 const express = require('express');
 require('dotenv').config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
   res.send(`${process.env.MESSAGE}`);
 });
--- src/config/SequelizeConfig.js
+++ src/config/SequelizeConfig.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize');
+
 const sequelize = new Sequelize('databasename', 'username', 'password', {
   host: 'localhost',
   dialect: 'postgres'
