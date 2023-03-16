fe61cdf9c383e8057d5d1275b4c7705c5724e451
Irakoze-Hortance <hortanceirakoze@gmail.com>
2023-03-16 11:49:41 +0200

    implemented the following functions: --google login --facebook login --handle login failure

---
+++ src/modules/user/routes/facebookLogin.js
@@ -0,0 +1,14 @@
+const passport = require("passport")
+
+module.exports={
+    getFacebookLogin:[passport.authenticate("facebook",{scope:["profile"]})],
+
+    handleFacebookLogin:[
+        passport.authenticate("facebook",{
+            failureRedirect:"/login"
+        }),
+        function(req,res){
+            res.redirect("/")
+        }
+    ]
+}
