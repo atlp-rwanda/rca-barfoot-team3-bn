46f0b26b15330c4fd9df79a0ecfe6747db8eed70
tracy8 <tracymurenzi@gmail.com>
2023-03-22 14:35:03 +0200

    added a line break

--- src/modules/user/routes/index.js
+++ src/modules/user/routes/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const { registerUser, loginUser, initateResetPassword, resetPassword, getUserById,
-  updateUserById, verifyUser } = require('../controller');
+const {
+   registerUser, loginUser, initateResetPassword, resetPassword, getUserById,
+  updateUserById, verifyUser
+ } = require('../controller');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
