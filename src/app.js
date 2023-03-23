541292524964b74ebf74f85483ec0232b3b6bd35
Blessing Hirwa <hirwablessing7@gmail.com>
2023-03-23 10:45:15 -0400

    chore: add some new methods in the user controller, other fixes

--- src/app.js
+++ src/app.js
@@ -7,9 +7,23 @@ const usersRouter = require('./modules/user/routes');
 const roleRoutes = require('./modules/role/routes');
 const permissionRoutes = require('./modules/permission/routes');
 const swaggerConfig = require('../swagger.json');
+const roleRoutes = require('./modules/role/routes');
+const permissionRoutes = require('./modules/permission/routes');
 
-const swaggerDocs = swaggerJsDocs(JSON.parse(JSON.stringify(swaggerConfig))); const app = express(); app.use(express.json()); const PORT = process.env.PORT || 3000; app.get('/', (req, res) => { res.send(`${process.env.MESSAGE}`); });
+const swaggerDocs = swaggerJsDocs(JSON.parse(JSON.stringify(swaggerConfig)));
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => { res.send(`${process.env.MESSAGE}`); });
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/roles', roleRoutes);
 app.use('/api/v1/permissions', permissionRoutes);
-app.listen(PORT, () => { dbClient.connect().then(() => { console.log('Connected to db'); app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, false, { docExpansion: 'none' })); console.log(`Example app listening on port ${PORT}!`); }); });
+app.listen(PORT, () => {
+    dbClient.connect().then(() => {
+        console.log('Connected to db');
+
+        app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, false, { docExpansion: 'none' }));
+        console.log(`Example app listening on port ${PORT}!`);
+    });
+});
