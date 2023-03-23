973f9fbbd5c2057528802af8cce9da67cc0630f9
izereuwonkunda <izereuwonkundakelia@gmail.com>
2023-03-23 09:35:36 +0200

    add:logout functionality

--- src/modules/user/routes/index.js
+++ src/modules/user/routes/index.js
@@ -1,15 +1,17 @@
 // user routes here
 const express = require('express');
+const { authenticate } = require('../../../middlewares/authenticate');
 
 const router = express.Router();
 
 const {
   registerUser, loginUser, getUserById,
-  updateUserById, verifyUser
+  updateUserById, verifyUser, logout
 } = require('../controller');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
+router.post('/logout', authenticate, logout);
 router.post('/verify/:email', verifyUser);
 router.get('/:id', getUserById);
 router.put('/:id', updateUserById);
