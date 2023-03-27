5fd25684f6d98fcf001ce99cf339513c799d8fdc
DeliceLydia <Delydia84@gmail.comm>
2023-03-27 14:52:19 +0200

    Fix linters

--- src/app.js
+++ src/app.js
@@ -10,9 +10,28 @@ require('./config/passport');
 require('dotenv').config();
 const { dbClient } = require('./database/index');
 const usersRouter = require('./modules/user/routes');
-const accommodationsRoute = require('./modules/accommodations/routes');
+const accommodationRouter = require('./modules/accommodations/routes');
 const tripRoute = require('./modules/trip/routes');
 const swaggerConfig = require('../swagger.json');
 
-const swaggerDocs = swaggerJsDocs(JSON.parse(JSON.stringify(swaggerConfig))); const app = express(); app.use(express.json()); const PORT = process.env.PORT || 3000; app.get('/', (req, res) => { res.send(`${process.env.MESSAGE}`); });
-app.use('/api/v1/users', usersRouter); app.use('/api/v1/accommodations', accommodationRouter); app.listen(PORT, () => { dbClient.connect().then(() => { console.log('Connected to db'); app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, false, { docExpansion: 'none' })); console.log(`Example app listening on port ${PORT}!`); }); });
+const swaggerDocs = swaggerJsDocs(JSON.parse(JSON.stringify(swaggerConfig)));
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send(`${process.env.MESSAGE}`);
+});
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/accommodations', accommodationRouter);
+app.use('/api/v1/trip', tripRoute);
+app.use('/', routes);
+app.listen(PORT, () => {
+  dbClient.connect().then(() => {
+    console.log('Connected to db');
+    app.use(
+      '/v1/api-docs',
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerDocs, false, { docExpansion: 'none' })
+    );
+    console.log(`Example app listening on port ${PORT}!`);
+  });
+});
