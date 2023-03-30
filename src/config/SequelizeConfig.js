9fe085af96fc5391bc31a7bcfcf63d4b1d00f1fb
DeliceLydia <Delydia84@gmail.comm>
2023-03-30 19:13:43 +0200

    Update README

--- src/config/SequelizeConfig.js
+++ src/config/SequelizeConfig.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  'team3nomad_db',
-  'cmkeny',
-  'Cmk@2023',
-  { host: 'localhost', dialect: 'postgres', logging: false }
+  'postgres',
+  'postgres',
+  'KWdFegey5wuSrP2',
+  { host: 'db.ndwotyychzlbilxjrnso.supabase.co', dialect: 'postgres', logging: false }
 ); module.exports = sequelize;
 
 // Uncomment this line after making changes to a model
