3c46ad77ca259ae9736ffcbace1bebcf9a4cfa1f
DeliceLydia <Delydia84@gmail.comm>
2023-04-04 11:38:52 +0200

    Fix linters

--- src/config/SequelizeConfig.js
+++ src/config/SequelizeConfig.js
@@ -3,8 +3,8 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   'postgres',
   'postgres',
-  'KWdFegey5wuSrP2',
-  { host: 'db.ndwotyychzlbilxjrnso.supabase.co', dialect: 'postgres', logging: false }
+  'ingabire',
+  { host: 'localhost', dialect: 'postgres', logging: false }
 ); module.exports = sequelize;
 
 // Uncomment this line after making changes to a model
