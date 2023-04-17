dbf3297e4aebb31057897b2ba1e29a4a5c79eab1
Anselme Habumugisha Irumva <andesanselme@gmail.com>
2023-04-17 17:08:40 +0200

    return an empty list instead of error on search into bookings

--- src/modules/booking/controllers/booking-controller.js
+++ src/modules/booking/controllers/booking-controller.js
@@ -436,12 +436,7 @@ class BookingController {
           as: 'onewaytrip'
         }
       });
-      if (bookings.length === 0) {
-        return res.status(404).json({
-          status: 404,
-          error: 'No bookings found related to your search'
-        });
-      }
+
       return res.status(200).json({
         status: 200,
         message: 'Bookings retrieved successfully',
