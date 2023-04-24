d393a562c0c6fd3b3e39f8425d2834b9d773331b
Blessing Hirwa <hirwablessing7@gmail.com>
2023-04-24 11:27:04 +0200

    fix: allow managers and admin to only get notifications by type and booking id

--- src/modules/notification/routes/index.js
+++ src/modules/notification/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const router = express.Router();
 const { authenticate } = require('../../../middlewares/authenticate');
+const { authorize } = require("../../../middlewares/authorize")
 const { NotificationsController } = require('../controllers');
 
 /**
@@ -200,6 +201,6 @@ router.get('/:userId', [authenticate], NotificationsController.getNotificationsB
  */
 router.delete('/:id', [authenticate], NotificationsController.deleteNotification);
 
-router.get('/:type/:bookingId', [authenticate], NotificationsController.getNotificationsByBookingAndType);
+router.get('/:type/:bookingId', [authenticate, authorize("MANAGER", "ADMIN")], NotificationsController.getNotificationsByBookingAndType);
 
 module.exports = router;
